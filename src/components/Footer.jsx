fbac410123974258a5847ce7a7805f2705ef1810
Ismael Santana Teixeira <art0066438@aluno.firjansenaisesi.com.br>
2026-03-06 16:09:58 -0300

    Novas auterações

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer(){
         <footer style={{background: '#f1edff', padding: '20px'}}>
             <h2>Novíssima ltda</h2>
             <h3>Desde 2026</h3>
-            <a href="" style={{color: 'black'}}>Facebook</a><br />
-            <a href="" style={{color: 'black'}}>Instagram</a><br />
-            <a href="" style={{color: 'black'}}>Linkedln</a>
+            <a href="https://www.bing.com/ck/a?!&&p=34194457eb0659e60f9482b1a33bae8c0fd315c975cf879e1f26dcac37c1a97aJmltdHM9MTc3Mjc1NTIwMA&ptn=3&ver=2&hsh=4&fclid=2fb2a598-72e5-606c-0f50-b28c738061b9&psq=facebook&u=a1aHR0cHM6Ly9wdC1ici5mYWNlYm9vay5jb20v" style={{color: 'black'}}>Facebook</a><br />
+            <a href="https://www.bing.com/ck/a?!&&p=d65c3199b08c9e1363b1c7563d36f80c2459a5042116a0aab592ee7e1b7224c0JmltdHM9MTc3Mjc1NTIwMA&ptn=3&ver=2&hsh=4&fclid=2fb2a598-72e5-606c-0f50-b28c738061b9&psq=instagram&u=a1aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS8" style={{color: 'black'}}>Instagram</a><br />
+            <a href="https://www.bing.com/ck/a?!&&p=d50850f1a26490c3c3ef6b6e61e3507f92a72053328485bf9a34a4d84aa9631dJmltdHM9MTc3Mjc1NTIwMA&ptn=3&ver=2&hsh=4&fclid=2fb2a598-72e5-606c-0f50-b28c738061b9&psq=linkedln&u=a1aHR0cHM6Ly9ici5saW5rZWRpbi5jb20v" style={{color: 'black'}}>Linkedln</a>
         </footer>
     )
 }
