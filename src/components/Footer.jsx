2f6527cb79a49ec55b64c771fb0d6574d2f32e0a
Ismael Santana Teixeira <art0066438@aluno.firjansenaisesi.com.br>
2026-03-06 16:27:02 -0300

    Novas auteraç

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
 
     return(
-        <footer style={{background: '#f1edff', padding: '20px'}}>
+        <footer style={{background: '#f1edff', padding: '20px', position: 'absolute', bottom: 0, width: '100%'}}>
             <h2>Novíssima ltda</h2>
             <h3>Desde 2026</h3>
             <a href="https://www.bing.com/ck/a?!&&p=34194457eb0659e60f9482b1a33bae8c0fd315c975cf879e1f26dcac37c1a97aJmltdHM9MTc3Mjc1NTIwMA&ptn=3&ver=2&hsh=4&fclid=2fb2a598-72e5-606c-0f50-b28c738061b9&psq=facebook&u=a1aHR0cHM6Ly9wdC1ici5mYWNlYm9vay5jb20v" style={{color: 'black'}}>Facebook</a><br />
